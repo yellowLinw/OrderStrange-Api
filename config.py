b8d206b6a31677f8f90de61f29b9d674bd46d0e7
hyl <852701957@qq.com>
2018-12-20 14:38:39 +0800

    创建数据库连接,flask-migrate管理数据库

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-class Config(object):
-    pass
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class Config():
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
